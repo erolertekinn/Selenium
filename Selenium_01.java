8c563f3457acbd15389df2a471f4c98c23413a28
Erol Ertekin <erolertekinn.66@gmail.com>
2020-11-25 18:24:02 +0100

    Selenium

---
+++ Selenium_01.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium_01 {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\erole\\Masaüstü\\Eclipse Workspace\\Chrome-Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.java.com./tr/");
+		//driver.manage().window().maximize();
+		
+		
+	}
+}
