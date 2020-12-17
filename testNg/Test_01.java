be2bcaa2f07cabb1084c5eea5abec7b2b562f6ee
Erol Ertekin <erolertekinn.66@gmail.com>
2020-12-17 22:30:40 +0100

    Selenium

---
+++ testNg/Test_01.java
@@ -0,0 +1,38 @@
+package testNg;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Test_01 {
+  @Test
+  public void test1() {
+	  System.out.println("ilk test");
+  }
+  @Test
+  public void test2() {
+	  System.out.println("ikinci test");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("Metod'dan önce");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("Metod'dan sonra");
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("Test'den önce");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("Test'den sonra");
+  }
+
+}
